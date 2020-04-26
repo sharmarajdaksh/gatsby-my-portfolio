2e9df8bbd5d1605acb85af32b5d48f5515ed2728
Travis CI User <travis@example.org>
2020-04-26 07:45:15 +0000

    Updates

--- component---src-pages-404-js-281587ec62a7c03a698f.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),u=t("ReeX"),o=t("9JMG"),r=function(){return l.a.createElement(o.a,null,l.a.createElement(u.a,{title:"Page Not Found"}),l.a.createElement("h1",null,"Page not found!"))};n.default=function(){return l.a.createElement(r,null)}}}]);
-//# sourceMappingURL=component---src-pages-404-js-281587ec62a7c03a698f.js.map
