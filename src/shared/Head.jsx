c1e73c978776679b642ed3a5227bccc7b858bebf
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2020-04-29 11:01:20 +0530

    Meta update

--- src/shared/Head.jsx
+++ src/shared/Head.jsx
@@ -14,5 +14,49 @@ export default ({ title }) => {
         }
     `)
 
-    return <Helmet title={`${title} | ${q.site.siteMetadata.title}`} />
+    return (
+        <Helmet>
+            <meta
+                name="title"
+                content={`${title} | ${q.site.siteMetadata.title}`}
+            />
+            <meta
+                name="description"
+                content="Dakshraj Sharma's website and blog!"
+            />
+
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://sharmarajdaksh.xyz/" />
+            <meta
+                property="og:title"
+                content={`${title} | ${q.site.siteMetadata.title}`}
+            />
+            <meta
+                property="og:description"
+                content="Dakshraj Sharma's website and blog!"
+            />
+            <meta
+                property="og:image"
+                content="https://sharmarajdaksh.xyz/coder_on_desk.svg"
+            />
+
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta
+                property="twitter:url"
+                content="https://sharmarajdaksh.xyz/"
+            />
+            <meta
+                property="twitter:title"
+                content={`${title} | ${q.site.siteMetadata.title}`}
+            />
+            <meta
+                property="twitter:description"
+                content="Dakshraj Sharma's website and blog!"
+            />
+            <meta
+                property="twitter:image"
+                content="https://sharmarajdaksh.xyz/coder_on_desk.svg"
+            />
+        </Helmet>
+    )
 }
